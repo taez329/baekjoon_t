ae28a9cbf46977347bef10e5c3431e4b66b67cf8
taez329 <xowjd0329@naver.com>
2024-01-05 14:58:30 +0000

    단계별 1차원 배열 24.01.05.

--- 10807.cpp
+++ 10807.cpp
@@ -8,7 +8,7 @@ int main()
 	
 	
 	cin>>N;
-	int *str = new int[N]; //NULL 고려해서 크기 할당해주기!!
+	int *str = new int[N+1]; //NULL 고려해서 크기 할당해주기!!
 	
 	for (int i=0; i<N; i++)
 		cin>>str[i];
@@ -18,7 +18,8 @@ int main()
 	for (int i=0; i<N; i++)
 		if (str[i]==v) num++; // 배열을 char형으로 선언하면 int와 char의 비교라 비교가 안됨 int형 배열로 해줘야함
 	cout<<num<<'\n';
-	/*
+	/*2
+	
 	while (*str!='\0')
 	{
 		if (*str==v)
